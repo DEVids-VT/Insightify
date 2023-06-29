a10c0840dc296358375acf44f98b555250c1b6d2
David Hristov <orionvt3@gmail.com>
2023-06-29 16:05:06 +0300

    added specifications

---
+++ src/Services/Insightify.Posts/Insightify.Posts.Domain/Insightify.Posts.Domain/Posts/Specifications/PostAuthorIdSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Insightify.Posts.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Insightify.Posts.Domain.Posts.Models;
+
+namespace Insightify.Posts.Domain.Posts.Specifications
+{
+    public class PostAuthorIdSpecification : Specification<Post>
+    {
+        private readonly string? authorId;
+
+        public PostAuthorIdSpecification(string? authorId) => this.authorId = authorId;
+
+        protected override bool Include => this.authorId != null;
+        public override Expression<Func<Post, bool>> ToExpression() => post => post.AuthorId == this.authorId!;
+    }
+
+}
