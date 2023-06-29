6395062010786817207c2f4f1ccf49bd85db3faa
David Hristov <orionvt3@gmail.com>
2023-06-29 13:49:14 +0300

    added domain configuration

---
+++ src/Services/Insightify.Posts/Insightify.Posts.Domain/Insightify.Posts.Domain/DomainConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Insightify.Posts.Domain
+{
+    using Insightify.Posts.Domain.Common;
+    using Microsoft.Extensions.DependencyInjection;
+    public static class DomainConfiguration
+    {
+        public static IServiceCollection AddDomain(this IServiceCollection services)
+            => services
+                .AddFactories();
+
+        private static IServiceCollection AddFactories(this IServiceCollection services)
+            => services
+                .Scan(scan => scan
+                    .FromCallingAssembly()
+                    .AddClasses(classes => classes
+                        .AssignableTo(typeof(IFactory<>)))
+                    .AsMatchingInterface()
+                    .WithTransientLifetime());
+    }
+}
