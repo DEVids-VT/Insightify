ea0aeee103ec1286449e9b1c16fbad44165c5eb7
David Hristov <orionvt3@gmail.com>
2023-07-11 11:48:32 +0300

    Added email confirmation on register

---
+++ src/Services/Insightify.IdentityAPI/Insightify.IdentityAPI/EmailSending/EmailMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Insightify.IdentityAPI.EmailSending
+{
+    public record EmailMessage
+    {
+        public string To { get; init; } = default!;
+        public string Subject { get; init; } = default!;
+        public string Content { get; init; } = default!;
+    }
+}
