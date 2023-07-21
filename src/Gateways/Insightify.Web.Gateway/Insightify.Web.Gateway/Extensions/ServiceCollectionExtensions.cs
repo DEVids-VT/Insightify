8ce0d3b8ff69769bfaa7db56961482623c06e3ec
David Hristov <orionvt3@gmail.com>
2023-07-21 13:54:21 +0300

    created project

---
+++ src/Gateways/Insightify.Web.Gateway/Insightify.Web.Gateway/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Insightify.Web.Gateway.Extensions
+{
+    [ExcludeFromCodeCoverage]
+
+    public static class ServiceCollectionExtensions
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+        public static IServiceCollection AddCustomAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var identityUrl = configuration.GetValue<string>("IdentityUrl");
+
+            services.AddAuthentication("Bearer").AddJwtBearer(options => {
+                options.Authority = identityUrl;
+                options.RequireHttpsMetadata = false;
+                options.Audience = "gateway";
+                options.TokenValidationParameters.ValidateAudience = false;
+            });
+            services.AddAuthorization(options => {
+                options.AddPolicy("ApiScope", policy => {
+                    policy.RequireAuthenticatedUser();
+                    policy.RequireClaim("scope", "gateway");
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/Gateways/Insightify.Web.Gateway/Insightify.Web.Gateway/Program.cs
@@ -0,0 +1,84 @@
+using System.Diagnostics.CodeAnalysis;
+using Insightify.Framework;
+using Insightify.Framework.HealthChecks;
+using Insightify.Framework.Logging;
+using Insightify.Framework.Pagination.Headers;
+using Insightify.Framework.Swagger;
+using Insightify.Framework.Swagger.Settings;
+using Insightify.Web.Gateway.Extensions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ApplicationName = typeof(Program).Assembly.FullName,
+    ContentRootPath = Directory.GetCurrentDirectory()
+});
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+var swaggerSettings = builder.Configuration.GetSection("Swagger").Get<SwaggerSettings>();
+
+builder.Services.AddCoreServices(coreBuilder =>
+{
+    var oAuthSettings = builder.Configuration.GetSection("SwaggerSecurity").Get<OAuthSecuritySettings>();
+    coreBuilder.AddSwagger(p =>
+    {
+        p.LoadSettingsFrom(swaggerSettings);
+        p.LoadSecuritySettingsFrom(oAuthSettings);
+    });
+
+    coreBuilder.AddHealthChecks(p =>
+    {
+
+    });
+
+}, enforceAuthorization: true);
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<PaginationHeadersFilter>();
+}).AddNewtonsoftJson(opts =>
+{
+    opts.SerializerSettings.Converters.Add(
+        new StringEnumConverter(new DefaultNamingStrategy(), false));
+});
+
+builder.Services
+    .AddApplicationServices(builder.Configuration)
+    .AddCustomAuthentication(builder.Configuration);
+
+builder.Host.UseLogging(p =>
+{
+    p.WithConsoleSink(true);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseSwagger(swaggerSettings!);
+
+// Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseCors("CorsAllowAllOrigins");
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.UseHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+});
+
+app.Run();
+
+[ExcludeFromCodeCoverage]
+public partial class Program
+{
+    public static string Namespace = typeof(Program).Assembly.GetName().Name;
+    public static string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
+}
