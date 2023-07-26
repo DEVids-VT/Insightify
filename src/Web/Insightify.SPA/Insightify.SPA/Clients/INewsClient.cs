ae895d161f41cade2b033b5586f6f0b4bdb6b8c9
David Hristov <orionvt3@gmail.com>
2023-07-26 12:25:49 +0300

    basic client implementation and displaying articles on fe

---
+++ src/Web/Insightify.SPA/Insightify.SPA/Clients/INewsClient.cs
@@ -0,0 +1,11 @@
+﻿using Insightify.SPA.Models;
+using Refit;
+
+namespace Insightify.SPA.Clients
+{
+    public interface INewsClient
+    {
+        [Get("/news")]
+        Task<IApiResponse<IEnumerable<NewsArticle>>> Articles([Query] int pageIndex = 1, [Query] int pageSize = 50);
+    }
+}
