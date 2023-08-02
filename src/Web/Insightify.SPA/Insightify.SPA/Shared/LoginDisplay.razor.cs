2ea82d2e7bb6a4222343bab5c2fa65c6c4739d3f
David Hristov <orionvt3@gmail.com>
2023-08-02 15:34:23 +0300

    added basic login top navbar and notification ico

---
+++ src/Web/Insightify.SPA/Insightify.SPA/Shared/LoginDisplay.razor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Insightify.SPA.Shared
+{
+    public partial class LoginDisplay
+    {
+
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+
+        private bool isDropdownVisible = false;
+
+        public void OpenCloseDropdown()
+        {
+            isDropdownVisible = !isDropdownVisible;
+        }
+        
+        private ElementReference header;
+        private string headerClass = "";
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var dotNetRef = DotNetObjectReference.Create(this);
+                await JSRuntime.InvokeVoidAsync("addScrollListener", dotNetRef);
+            }
+        }
+
+        [JSInvokable]
+        public void MakeHeaderSticky()
+        {
+            headerClass = "login-sticky";
+            StateHasChanged();
+        }
+
+        [JSInvokable]
+        public void MakeHeaderUnSticky()
+        {
+            headerClass = "";
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            JSRuntime.InvokeVoidAsync("removeScrollListener");
+        }
+    }
+}
+
