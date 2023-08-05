f1b71cc5dfb5442cfb5d6d32ba3e1bf4e16f74b6
David Petkov <davidpetkov2006@gmail.com>
2023-08-05 16:14:46 +0300

    Created Insighify.FinancialDataApi

---
+++ src/Services/Insighify.FinancialDataApi/Insighify.FinancialDataApi/Controllers/CryptoDataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Insighify.FinancialDataApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CryptoDataController : ControllerBase
+    {
+        [HttpGet("{coinId}")]
+        public IActionResult GetCryptoData(string coinId, [FromQuery] bool includeDeleted)
+        {
+            var result = new List<string> { "bitcoin", "etherium" };
+
+            return Ok(result);
+        }
+    }
+}
