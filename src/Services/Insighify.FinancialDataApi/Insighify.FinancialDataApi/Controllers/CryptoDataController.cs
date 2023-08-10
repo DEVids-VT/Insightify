0495f4f3337237f7304096c148c216af980cfafd
David Petkov <davidpetkov2006@gmail.com>
2023-08-10 14:48:13 +0300

    Did some changes

--- src/Services/Insighify.FinancialDataApi/Insighify.FinancialDataApi/Controllers/CryptoDataController.cs
+++ src/Services/Insighify.FinancialDataApi/Insighify.FinancialDataApi/Controllers/CryptoDataController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Insighify.FinancialDataApi.Configuration;
+using Insighify.FinancialDataApi.ResponceModels.Crypto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Insighify.FinancialDataApi.Controllers
 {
@@ -6,12 +9,24 @@ namespace Insighify.FinancialDataApi.Controllers
     [Route("[controller]")]
     public class CryptoDataController : ControllerBase
     {
-        [HttpGet("{coinId}")]
-        public IActionResult GetCryptoData(string coinId, [FromQuery] bool includeDeleted)
+        [HttpGet("/historicalData")]
+        public async Task<IActionResult> GetHistoricalData(string id, string currency, string days, string interval, string precision)
         {
-            var result = new List<string> { "bitcoin", "etherium" };
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(UrlsConfig.CryptoCoinsOpperations.GetHistoricalDataByCoinId(id, currency, days, interval, precision));
 
-            return Ok(result);
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    var coinHistory = JsonConvert.DeserializeObject<CoinHistory>(jsonResponse);
+                    return Ok(coinHistory);
+                }
+                else
+                {
+                    throw new Exception($"Failed to fetch data. Status code: {response.StatusCode}");
+                }
+            }
         }
     }
 }
--- src/Services/Insighify.FinancialDataApi/Insighify.FinancialDataApi/Program.cs
+++ src/Services/Insighify.FinancialDataApi/Insighify.FinancialDataApi/Program.cs
@@ -6,9 +6,13 @@ using Insightify.Framework.Automapper;
 using Insightify.Framework.Fetching.Extensions;
 using Insightify.Framework.HealthChecks;
 using Insightify.Framework.MongoDb.Abstractions.Configuration;
+using Insightify.Framework.Pagination.Headers;
+using Insightify.Framework.Swagger;
 using Insightify.Framework.Swagger.Settings;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using MongoDB.Bson;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 using Serilog;
 using System.Reflection;
 
@@ -16,13 +20,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddCustomConfiguration();
 
+var mongoDbSettings = builder.Configuration.GetSection("Mongo").Get<MongoSettings>();
+var swaggerSettings = builder.Configuration.GetSection("Swagger").Get<SwaggerSettings>();
+var oAuthSettings = builder.Configuration.GetSection("SwaggerSecurity").Get<OAuthSecuritySettings>();
+
 builder.Services.AddCoreServices(coreBuilder =>
 {
     var assembly = typeof(Program).Assembly;
-
-    var mongoDbSettings = builder.Configuration.GetSection("Mongo").Get<MongoSettings>();
-    var swaggerSettings = builder.Configuration.GetSection("Swagger").Get<SwaggerSettings>();
-    var oAuthSettings = builder.Configuration.GetSection("SwaggerSecurity").Get<OAuthSecuritySettings>();
 
     coreBuilder.AddMongoDatabase(p =>
     {
@@ -37,25 +41,35 @@ builder.Services.AddCoreServices(coreBuilder =>
         p.WithIgnoreIfDefaultConvention(false);
         p.WithIgnoreIfNullConvention(true);
     });
-
     coreBuilder.AddAutomapper(assembly);
-
     coreBuilder.AddHealthChecks(p =>
     {
         p.AddMongoDb(mongoDbSettings!.Url);
     });
-
-    coreBuilder.AddApiFetcher(p =>
+    coreBuilder.AddSwagger(p =>
     {
-        var key = builder.Configuration.GetSection("LiveNewsApi").GetValue<string>("Key")!;
-        var url = builder.Configuration.GetSection("LiveNewsApi").GetValue<string>("Url")!;
-        p.WithName("LiveNewsApi");
-        p.WithApiKey(key, "access_key");
-        p.WithBaseUrl(url);
+        p.LoadSettingsFrom(swaggerSettings);
+        p.LoadSecuritySettingsFrom(oAuthSettings);
     });
+    /*    coreBuilder.AddApiFetcher(p =>
+        {
+            var key = builder.Configuration.GetSection("LiveNewsApi").GetValue<string>("Key")!;
+            var url = builder.Configuration.GetSection("LiveNewsApi").GetValue<string>("Url")!;
+            p.WithName("LiveNewsApi");
+            p.WithApiKey(key, "access_key");
+            p.WithBaseUrl(url);
+        });*/
 });
-
-builder.AddQuartzNET();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<PaginationHeadersFilter>();
+})
+.AddNewtonsoftJson(opts =>
+{
+    opts.SerializerSettings.Converters.Add(
+        new StringEnumConverter(new DefaultNamingStrategy(), false));
+});
+//builder.AddQuartzNET();
 builder.Services.AddValidatorsFromAssembly(Assembly.Load(Namespace), ServiceLifetime.Scoped);
 
 builder.AddApplicationServices();
@@ -68,6 +82,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseRouting();
+
+app.UseSwagger(swaggerSettings!);
 
 app.MapHealthChecks("/hc", new HealthCheckOptions()
 {
